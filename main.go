5cbb081592b3a1d750eed4e6fce10bdfa9262f30
Andreas Thomas <dev@chronark.com>
2026-01-25 14:52:12 +0100

    Update response message to include 'hello demo:'

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ func main() {
 	}
 	
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte(time.Now().Format(time.RFC3339)))
+		w.Write([]byte("hello demo: " + time.Now().Format(time.RFC3339)))
 	})
 	http.ListenAndServe(":"+port, nil)
 }
