93fe2e124388025c00c6b9fe456387a63b54ef49
chronark <dev@chronark.com>
2026-02-03 08:38:43 +0100

    feat: new subdir

---
+++ sub/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"net/http"
+	"os"
+	"time"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("sub" +time.Now().Format(time.RFC3339)))
+	})
+	http.ListenAndServe(":"+port, nil)
+}
