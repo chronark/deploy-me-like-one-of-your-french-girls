d4f97db9c7c33ad8d1cce7f45b94f734554670f6
chronark <dev@chronark.com>
2026-02-11 16:19:06 +0100

    fix: log env

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	"time"
 	"log"
+	"encoding/json"
 )
 
 func main() {
@@ -12,16 +13,19 @@ func main() {
 	if port == "" {
 		port = "8080"
 	}
-	
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		log.Println("received request")
 		w.Write([]byte("deployed through redeploy2: " + time.Now().Format(time.RFC3339)))
 	})
 
 	http.HandleFunc("/env", func(w http.ResponseWriter, r *http.Request) {
-		
-		w.Write([]byte(os.Environ()))
+
+		b, _ := json.Marshal(os.Environ())
+
+
+		w.Write(b)
 	})
-	
+
 	http.ListenAndServe(":"+port, nil)
 }
