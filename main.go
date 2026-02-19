3002b41db2d162a9a5da2030f3b668d1de9bb908
Andreas Thomas <dev@chronark.com>
2026-02-19 17:43:31 +0100

    Update main.go

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ func main() {
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		log.Println("received reques2t ")
-		w.Write([]byte("pls deploy on unkey: " + time.Now().Format(time.RFC3339)))
+		w.Write([]byte("pls dep2loy on unkey: " + time.Now().Format(time.RFC3339)))
 	})
 
 	http.HandleFunc("/env", func(w http.ResponseWriter, r *http.Request) {
