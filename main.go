6a123f1f78e34dc6e5118e40b061e32716943ab4
chronark <dev@chronark.com>
2026-03-13 13:14:46 +0100

    show headers

--- main.go
+++ main.go
@@ -20,6 +20,20 @@ func main() {
 		w.Write([]byte("hello 44222 I hate regions!!! did flo break krane? " + "host="+r.URL.Host + time.Now().Format(time.RFC3339)))
 	})
 
+
+
+	http.HandleFunc("/v1/headers", func(w http.ResponseWriter, r *http.Request) {
+
+		b, err := json.Marshal(r.Header)
+		if err != nil {
+			log.Println("error marshaling headers: ", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		w.Write(b)
+	})
+
 	http.HandleFunc("/v1/abc", func(w http.ResponseWriter, r *http.Request) {
 		log.Println( r.URL.Path)
 		w.Write([]byte("hello from /v1/abc"))
