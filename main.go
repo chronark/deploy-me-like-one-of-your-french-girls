d52754944416dc938855b7887029aa7f9826eddd
chronark <dev@chronark.com>
2026-03-13 12:59:58 +0100

    more stuff

--- main.go
+++ main.go
@@ -12,14 +12,27 @@ func main() {
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "3000"
-	} 
-	
+	}
+
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		log.Println("received rseques2t ")
 		w.Write([]byte("hello 44222 I hate regions!!! did flo break krane? " + "host="+r.URL.Host + time.Now().Format(time.RFC3339)))
 	})
 
+	http.HandleFunc("/v1/abc", func(w http.ResponseWriter, r *http.Request) {
+		log.Println( r.URL.Path)
+		w.Write([]byte("hello from /v1/abc"))
+	})
+	http.HandleFunc("/v1/def", func(w http.ResponseWriter, r *http.Request) {
+		log.Println( r.URL.Path)
+		w.Write([]byte("hello from /v1/def"))
+	})
+
+	http.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
+		log.Println( r.URL.Path)
+		w.Write([]byte("hello from /v1/def"))
+	})
 	http.HandleFunc("/env", func(w http.ResponseWriter, r *http.Request) {
 
 		b, _ := json.Marshal(os.Environ())
