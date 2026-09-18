6634f3497bb04cfab84446cee514c69605998d24
Andreas Thomas <dev@chronark.com>
2026-09-18 16:46:30 +0200

    Update main.go

--- sub/main.go
+++ sub/main.go
@@ -14,5 +14,6 @@ func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("sub" + time.Now().Format(time.RFC3339)))
 	})
+	
 	http.ListenAndServe(":"+port, nil)
 }
